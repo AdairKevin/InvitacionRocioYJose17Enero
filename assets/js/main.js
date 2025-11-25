373322db25650afcd8378835b580cb52ebddc596
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2025-11-25 11:24:03 -0600

    mexico

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const DATE_TARGET = new Date("11/15/2025 01:30 PM");
+  const DATE_TARGET = new Date("01/17/2026 01:30 PM");
 
   const SPAN_DAYS = document.querySelector("span#days");
   const SPAN_HOURS = document.querySelector("span#hours");
